1b3715a0fa754506bf8c1b5f5d4699619b27f5a8
Alex P <Alex.peschel@gmail.com>
2022-04-05 00:28:32 +0200

    Just commented out the Core.h because it is already called in EventMain.h

--- GameEngine/src/GameEngine/Application.h
+++ GameEngine/src/GameEngine/Application.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Core.h"
+//#include "Core.h"
 #include "InputEvents/EventMain.h"
 #include "GameEngine/InputEvents/WindowEvents.h"
 #include "GameEngine/InputEvents/MouseEvents.h"
