3d124519fb500d1e106f3ba97adf37101b672d01
Raphaël Dupont <72378617+Quittqick@users.noreply.github.com>
2022-04-18 14:16:43 +0200

    add Vector3 to MathHelper.h

--- GameEngine/src/GameEngine/MathHelper.h
+++ GameEngine/src/GameEngine/MathHelper.h
@@ -32,4 +32,34 @@ namespace GameEngine{
 	inline Vector2 operator -(const Vector2& lhs, const Vector2& rhs) {
 		return Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
 	}
+
+	struct Vector3 {
+		float x;
+		float y;
+		float z;
+
+		Vector3(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f)
+			:x(_x), y(_y), z(_z) {}
+
+		float  MagnitudeSqr() {
+			return x * x + y * y + z * z;
+		}
+
+		float Magnitude() {
+			return (float)sqrt( MagnitudeSqr() );
+		}
+
+		Vector3 Normalized() {
+			float mag = Magnitude();
+			return Vector3(x / mag, y / mag, z / mag);
+		}
+	};
+
+	inline Vector3 operator +(const Vector3& lhs, const Vector3& rhs) {
+		return Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+	}
+
+	inline Vector3 operator -(const Vector3& lhs, const Vector3& rhs) {
+		return Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+	}
 }
