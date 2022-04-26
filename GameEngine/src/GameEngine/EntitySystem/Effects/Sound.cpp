f88f63fa3554db4f1300896e6d0ef100cf5d86b8
oliolo <oliver_olofsson@hotmail.se>
2022-04-26 15:20:40 +0200

    added some basic functions

--- GameEngine/src/GameEngine/EntitySystem/Effects/Sound.cpp
+++ GameEngine/src/GameEngine/EntitySystem/Effects/Sound.cpp
@@ -10,3 +10,28 @@ GameEngine::Sound::~Sound() {
 void GameEngine::Sound::Update() {
 
 }
+
+void GameEngine::Sound::playSound() {
+
+}
+
+void GameEngine::Sound::stopChannel() {
+
+}
+
+void GameEngine::Sound::setSoundVolume() {
+
+}
+
+void GameEngine::Sound::playMusic() {
+
+}
+
+void GameEngine::Sound::stopMusic() {
+
+}
+
+void GameEngine::Sound::setMusicVolume() {
+
+}
+
--- GameEngine/src/GameEngine/EntitySystem/Effects/Sound.h
+++ GameEngine/src/GameEngine/EntitySystem/Effects/Sound.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../BaseComponent.h"
+#include <SDL_mixer.h>
 
 namespace GameEngine {
 	class Sound : public BaseComponent {
@@ -7,5 +8,13 @@ namespace GameEngine {
 		Sound(const char* dir);
 		~Sound();
 		void Update() override;
+		
+		void playSound(const char* fileName);
+		void stopChannel();
+		void setSoundVolume();
+
+		void playMusic();
+		void stopMusic();
+		void setMusicVolume();
 	};
 }
