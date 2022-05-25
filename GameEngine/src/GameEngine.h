3c62efdf6fa54339ad5369ec62844b1c0a87e12d
Alex P <Alex.peschel@gmail.com>
2022-05-25 10:32:29 +0200

    some small changes

--- GameEngine/src/GameEngine.h
+++ GameEngine/src/GameEngine.h
@@ -1,16 +1,5 @@
 #pragma once
 
-// For use by GameEngine Applications
 #include "GameEngine/Application.h"
-#include "GameEngine/Log.h"
-
-#include "GameEngine/Camera/Camera.h"
-
-#include "GameEngine/EntitySystem/BaseComponent.h"
-#include "GameEngine/EntitySystem/GameObject.h"
-#include "GameEngine/Window/Graphics.h"
-#include "GameEngine/InputEvents/InputManager.h"
-#include "GameEngine/EntitySystem/EntityManager.h"
 
 #include <iostream>
-
